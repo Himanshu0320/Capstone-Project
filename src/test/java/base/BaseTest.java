681837f33942ba15edc68035c9d84a020b05d42b
Himanshu0320 <156709740+Himanshu0320@users.noreply.github.com>
2026-06-15 13:01:43 +0530

    Enhance BaseTest setup for headless browser support
    Updated setup method to use optional browser parameter and added options for headless execution in Chrome and Firefox.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,10 +1,15 @@
 package base;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,29 +20,44 @@ public class BaseTest {
 
     @Parameters("browser")
     @BeforeMethod
-    public void setup(String browser) {
+    public void setup(@Optional("chrome") String browser) {
 
         if (browser.equalsIgnoreCase("chrome")) {
 
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // Required for Jenkins
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(options);
 
         } else if (browser.equalsIgnoreCase("firefox")) {
 
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver();
+
+            FirefoxOptions options = new FirefoxOptions();
+
+            // Required for Jenkins
+            options.addArguments("--headless");
+
+            driver = new FirefoxDriver(options);
 
         } else {
-
-            throw new IllegalArgumentException(
-                "Invalid browser. Supported browsers are: chrome, firefox");
+            throw new IllegalArgumentException("Invalid browser: " + browser);
         }
 
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driver.get("https://blazedemo.com/");
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
 
         if (driver != null) {
